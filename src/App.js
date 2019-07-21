ad21ea5ce6b2148defe95035cfe29d56e068c4db
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-07-21 13:06:24 -0300

    fix routing error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './pages/Home';
 import Runes from './pages/Runes';
 import Header from './layout/Header'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Gems from './pages/Gems';
 import Runewords from './pages/Runewords';
 import CubeFormulas from './pages/CubeFormulas';
@@ -13,12 +13,14 @@ function App() {
     <div>
       <Router>
         <Header/>
-        <Route exact path={'/'} render={() => <Home/> } />
-        <Route exact path={'/runas'} render={() => <Runes /> } />
-        <Route exact path={'/gemas'} render={() => <Gems /> } />
-        <Route exact path={'/palabras_runicas'} render={() => <Runewords /> } />
-        <Route exact path={'/cubo'} render={() => <CubeFormulas /> } />
-        <Route render={() => <NotFoundPage />} />
+        <Switch>
+          <Route exact path={'/'} render={() => <Home/> } />
+          <Route exact path={'/runas'} render={() => <Runes /> } />
+          <Route exact path={'/gemas'} render={() => <Gems /> } />
+          <Route exact path={'/palabras_runicas'} render={() => <Runewords /> } />
+          <Route exact path={'/cubo'} render={() => <CubeFormulas /> } />
+          <Route render={() => <NotFoundPage />} />
+        </Switch>
       </Router>
     </div>
   );
