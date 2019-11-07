a467fb4be95b6c8a271d7ce282fa68b6443573b1
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-11-06 22:06:25 -0300

    update unique items to import them in data js file

--- src/shared/data.js
+++ src/shared/data.js
@@ -3,6 +3,7 @@ import JsonGems from '../data/gems.json';
 import JsonRunewords from '../data/runewords.json';
 import JsonCubeRecipes from '../data/cube_recipies.json';
 import sortBy from 'lodash/sortBy';
+import { JEWELRY, NORMAL_ITEMS, ELITE_ITEMS, EXCEPTIONAL_ITEMS, PJ_ITEMS } from '../data/uniqueItems'; 
 
 const findRuneByCode = (code) => JsonRunes.find((aRune) => aRune.code === code);
 const findGemByCode  = (code) => JsonGems.find((aGem) => aGem.code === code);
@@ -18,7 +19,7 @@ const UnorderedRunewords = JsonRunewords.map( (runeword) => Object.assign({},
 
 const Runewords = sortBy(UnorderedRunewords, ['level', 'name']);
 
-const TIPS = {
+const Tips = {
     fireSkills: 'Icluye todo el árbol Fuego de hechicera, así como habilidades de amazona (flecha de fuego, flecha explosiva, flecha inmoladora), de paladín (fuego sagrado), de druida (garras de fuego, tormenta de fuego, roca líquida, fisura, volcán, armagedón), de asesina (puños de fuego, cola de dragón, ráfaga de fuego, estela de fuego, estela infernal), y de nigromante (golem de fuego, explosión de cadáveres).',
     damageOnMana: "Es un modificador que sirve para recuperar cantidades de la preciada materia azul cada vez que eres dañado. Contrariamente a la creencia popular, NO reduce de ninguna manera los daños recibidos, sino que el porcentaje poseído se aplica sobre la cifra obtenida para calcular cuánto maná se recupera. De esta manera, si tenemos 50% de daño infligido repercute en el maná y nos llevamos 100 puntos de daño, perderíamos 100 puntos de vida (supongamos que no tenemos resistencias ni daños reducidos de ninguna clase) y, en caso de no tener nuestra orbe de maná completa, recuperaríamos de golpe 50 puntos de materia azul. Si la cantidad bruta de maná recuperado excede el total que podemos llegar a tener los puntos restantes se perderán.<br/>Daño infligido repercute en el maná sólamente se aplica cuando se reciben daños de tipo físico, sean hechos en combate cuerpo a cuerpo o a distancia. El modificador no será tenido en cuenta si nos atacan con magia, elementales, o veneno.",
     normalUnique: 'Son los objetos únicos que tienen como base un objeto normal (que se puede encontrar en los niveles de dificultad normal, pesadilla e infierno).',
@@ -26,4 +27,11 @@ const TIPS = {
     eliteUnique: 'Son los objetos que tienen como base un objeto élite (que se puede encontrar finalizando el nivel de dificultad pesadilla, y empieza a ser más común en el nivel infierno).'
 }
 
-export { RunesData, Gems, CubeRecipes, Runewords }
+const a = NORMAL_ITEMS;
+console.log(a);
+console.log(JEWELRY);
+console.log(ELITE_ITEMS)
+console.log(EXCEPTIONAL_ITEMS)
+console.log(PJ_ITEMS);
+
+export { RunesData, Gems, CubeRecipes, Runewords, Tips }
