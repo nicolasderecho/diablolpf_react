e04c3666466a430ecf4c78c51750c6f19b3e9ab8
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-11-11 00:30:46 -0300

    add more tips about area 85

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -40,7 +40,76 @@ const TipsPage = (props) => {
                     <p>
                     Son los objetos que tienen como base un objeto élite (que se puede encontrar finalizando el nivel de dificultad pesadilla, y empieza a ser más común en el nivel infierno).
                     </p>
-                </Column>                          
+                </Column>
+                <Column columnSize='full'>
+                    <Title className='white-text' spaced>Area 85</Title>
+                </Column>               
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>¿Qué son las áreas 85?</Subtitle>
+                    <p>
+                    Las áreas 85 son lugares en el juego donde es normal que los monstruos dejen caer al matarlos ciertos objetos que por su alto TC (treasure class) no serían posibles encontrarlos en dicha ubicación.
+                    </p>
+                </Column>    
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>¿Por qué fueron incrementados los TC de estas áreas?</Subtitle>
+                    <p>
+                    La explicación de este fenómeno es que en anteriores versiones se dejaron en desuso o eran muy poco concurridas las áreas que actualmente son consideradas como áreas 85, por lo que los desarrolladores del parche 1.10 decidieron subirle el TC del área en dichos lugares para que sean más concurridos.
+                    </p>
+                </Column>  
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>¿De donde viene el nombre “áreas 85”?</Subtitle>
+                    <p>
+                    En las áreas todos los monstruos poseen el nivel 87 con lo que podrán dejar caer todos los objetos de TC inferior o igual al 87, o sea, todos los objetos del juego. Los monstruos poseen nivel 87 ya que se le suma 2 al nivel por ser esbirros (85+2=87), los monstruos superúnicos, únicos o campeones suman 3 al nivel 85, consiguiendo así el nivel 88.
+                    </p>
+                </Column>        
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>¿Qué puedo conseguir en estas áreas?</Subtitle>
+                    <p>
+                    Todos los objetos del juego pueden ser obtenidos en dichas áreas y en especial de los monstruos únicos, superúnicos y campeones.
+                    </p>
+                </Column>
+                <Column columnSize='full'>
+                    <Title className='white-text' spaced>¿Cuales son las Areas 85 del juego?</Title>
+                </Column>                                                              
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>Acto 1</Subtitle>
+                    <p>
+                        - Foso niveles 1 y 2 ( Se accede desde Tierras Altas de Tamoe)<br/>
+                        - Mausoleo (Se accede desde el cementerio)
+                    </p>
+                </Column>   
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>Acto 2</Subtitle>
+                    <p>
+                        - Cubil de los gusanos nivel 3 (Se accede desde el Oasis Lejano)<br/>
+                        - Túneles antiguos (Se accede desde Ciudad Perdida)
+                    </p>
+                </Column>      
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>Acto 3</Subtitle>
+                    <p>
+                        - Cloacas nivel 2<br/>
+                        - Templo olvidado (Se accede desde Parte alta de Kurast)<br/>
+                        - El santuario en ruinas (Se accede desde Calzada de Kurast)<br/>
+                        - Relicario abandonado (Se accede desde Calzada de Kurast)
+                    </p>
+                </Column>   
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>Acto 4</Subtitle>
+                    <p>
+                        - Río de llamas<br/>
+                        - Santuario del caos
+                    </p>
+                </Column>    
+                <Column columnSize='full'>
+                    <Subtitle as={'h3'} className='orange-text'>Acto 5</Subtitle>
+                    <p>
+                        - Cima del Monte arreat<br/>
+                        - Torres del Homenaje, todos lo niveles<br/>
+                        - Trono de la Destrucción<br/>
+                        - Camara del Mundo de Piedra
+                    </p>
+                </Column>                                                    
             </Columns>
             <br/>
             <br/>
