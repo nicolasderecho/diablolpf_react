da61907206d02136e904d08c7c7e34267583668d
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-11-14 19:48:18 -0300

    move spinner.css

--- src/shared/Spinner.js
+++ src/shared/Spinner.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Spinner.scss';
 
 const Spinner = () => {
     return <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
