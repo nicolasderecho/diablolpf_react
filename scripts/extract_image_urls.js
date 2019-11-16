77331a8091ef97462b7b7f3beadf1628f057a218
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-11-16 12:34:48 -0300

    add images from d2mantix

---
+++ scripts/extract_image_urls.js
@@ -0,0 +1,28 @@
+//http://www.d2mantix.com/diablo2lod-items-unicos.html
+const cleanSymbols = (string) => string.replace(new RegExp('↵|\n', 'g'), '').replace(new RegExp('`|’', 'g'), '\'');
+const cleanString = (string) => cleanSymbols(string).trim().replace(/\s+/g, ' ');
+const htmlStringToText = (string) => {
+  const htmlElement = document.createElement('div');
+  htmlElement.innerHTML = string;
+  return cleanString(htmlElement.innerText);
+};
+
+var extractImages = (rootElement) => {
+
+  const rows = Array.from(rootElement.querySelectorAll('tbody > tr'));
+  return rows.map(row => {
+    const data = row.querySelector('td');
+    const dataRows = data.innerHTML.split('<br>');
+    const image = (data.querySelector('img') || {src: ''}).src;
+    return {
+      image: image,
+      name: htmlStringToText(dataRows[1]),
+      originalName: htmlStringToText(dataRows[2])
+    }
+  }).filter( element => element.image !== '')
+
+};
+
+var images = extractImages($0);
+
+JSON.stringify(images);
